e347a4674006aba05f31b459cae336e10d7b9b4b
신동호 <101622086+letsplaycoding@users.noreply.github.com>
2025-01-27 19:23:44 +0900

    Frontend Env Settings

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <h1>야스</h1>
+    <!-- 기본 페이지는 '/' 경로일 때만 "야스"가 보이고, 다른 페이지에서는 내용이 변경됨 -->
+    <h1 v-if="$route.path === '/'">야스</h1>
     <router-view></router-view> <!-- 라우트된 컴포넌트를 여기에 표시 -->
   </div>
 </template>
--- src/router/router.js
+++ src/router/router.js
@@ -1,19 +1,18 @@
-// src/router.js
 import { createRouter, createWebHistory } from 'vue-router';
 import App from '@/App.vue';
 import NoticeList from '@/components/Notice/NoticeList.vue';  // 공지사항 리스트 컴포넌트
 
-// import store from '@/store/store.js';
 const routes = [
     {
         path: '/',
         name: 'Home',
-        component: App,
+        component: App,  // '/' 경로에서만 App.vue 컴포넌트 사용
     },
     {
         path: '/notice/list',
         name: '공지사항 전체 조회창',
-        component: NoticeList },
+        component: NoticeList,  // '/notice/list' 경로에서 NoticeList.vue 사용
+    },
 ];
 
 const router = createRouter({
