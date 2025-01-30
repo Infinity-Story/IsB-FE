4e49afa3d2fba672125ee3816f3e1335b29d4182
신동호 <101622086+letsplaycoding@users.noreply.github.com>
2025-01-30 18:35:56 +0900

    #5 base page "MainPage"
    기본창인 메인 로그인창 구현

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import App from '@/App.vue';
-import NoticeList from '@/components/Notice/NoticeList.vue';  // 공지사항 리스트 컴포넌트
+import NoticeList from '@/components/Notice/NoticeList.vue';
+import MainLogin from "@/components/Login/MainLogin.vue";  // 공지사항 리스트 컴포넌트
 
 const routes = [
+    {
+      path: '/',
+      name: 'CommonLogin',
+      component: MainLogin,
+    },
     {
         path: '/notice/list',
         name: '공지사항 전체 조회창',
